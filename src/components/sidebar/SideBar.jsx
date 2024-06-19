26fddec33a490f6e443d0f872d129c7a11c60c47
KangSungLee <rkdtjd1020@naver.com>
2024-06-20 00:57:40 +0900

    blog css

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -2,22 +2,17 @@ import React from 'react';
 import SideBarMenu from './SideBarMenu';
 import SideBarIcon from './SideBarIcon';
 import '../../css/sidebar.css';
-import { Drawer } from '@mui/material';
+import { Box } from '@mui/material';
 import SideBarProfile from './SideBarProfile';
 
 export default function Sidebar() {
   return (
-    <Drawer
+    <Box
       className="sideBar"
-      variant="permanent"
-      classes={{
-        paper: "drawerPaper", 
-      }}
-      anchor="left"
     >
       <SideBarProfile />
       <SideBarMenu />
       <SideBarIcon />
-    </Drawer>
+    </Box>
   );
 }
