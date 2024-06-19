78a67f1addfb1f1468f66898418f9f99dd9b1fe3
KangSungLee <rkdtjd1020@naver.com>
2024-06-20 01:43:59 +0900

    blog

--- src/pages/Implementation.jsx
+++ src/pages/Implementation.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ImplementationList from "../components/ImplementationList";
+import { Container } from "@mui/material";
 
 export default function Implementation() {
   return (
-      <>
+      <Container>
         <ImplementationList/>
-      </>
+      </Container>
   );
 }
