f7d47f568b854dec681a219353ba76f9cff5ff65
kangsunglee2 <rkdtjd123120@gmail.com>
2024-06-21 20:38:16 +0900

    search

--- src/components/topbar/TopBarSearch.jsx
+++ src/components/topbar/TopBarSearch.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from '@mui/icons-material/Search';
 
 export default function TopBarSearch() {
+    const [searchWord, setSearchWord] = useState('');
+
+    const handleSearchChange = (e) => {
+        setSearchWord(e.target.value);
+    };
 
     return (
         <div className="searchContainer">
-            <input type="text" placeholder="검색어를 입력하세요" className="searchInput" />
+            <input 
+                type="text" 
+                placeholder="검색어를 입력하세요" 
+                className="searchInput" 
+                onChange={handleSearchChange}
+                value={searchWord}
+            />
             <button className="searchButton">
                 <SearchIcon />
             </button>
